2be0df941b63ba865e8d8644fdfa89a10ad17796
Claire <cconnachan@gmail.com>
2018-03-13 19:54:21 +0000

    river getter test passed

---
+++ river.rb
@@ -0,0 +1,15 @@
+class River
+
+attr_reader :name
+
+def initialize(name)
+  @name = name
+end
+
+
+
+
+
+
+
+end
--- specs/river_spec.rb
+++ specs/river_spec.rb
@@ -3,8 +3,17 @@ require_relative('../river.rb')
 
 class RiverTest < MiniTest::Test
 
+  def setup
+    @river1 = River.new("Amazon")
+    @river2 = River.new("Yukon")
+    @river3 = River.new("Nile")
+  end
 
-
+  def test_fish_getter()
+    assert_equal("Amazon", @river1.name)
+    assert_equal("Yukon", @river2.name)
+    assert_equal("Nile", @river3.name)
+  end
 
 
 
