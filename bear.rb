37fa36b4805b6133801a363cdd35723c619ebb16
Claire <cconnachan@gmail.com>
2018-03-14 21:24:39 +0000

    solved hunt_for_fish for bear class, passed tests

--- bear.rb
+++ bear.rb
@@ -3,18 +3,38 @@ class Bear
   attr_reader :name
   attr_accessor :stomach
 
-  def initialize(name, stomach)
+  def initialize(name, stomach = [])
     @name = name
-    @stomach = []
+    @stomach = stomach
   end
 
   def eat_a_fish(fish)
     @stomach << fish
   end
 
-  def is_starving
+  def is_starving?
     @stomach.empty?
   end
+
+  def hunt_for_fish(river)
+    if river.fish_count != 0
+      for fish in river.fish_in_river
+        @stomach << fish
+        break
+      end
+    end
+  end
+
+
+
+
+
+
+
+
+
+
+
 
   # def hunt_for_fish(river)
   #   if river.fish_count == 0
--- specs/bear_spec.rb
+++ specs/bear_spec.rb
@@ -6,14 +6,17 @@ require_relative('../river.rb')
 class BearTest < MiniTest::Test
 
   def setup
-    @bear1 = Bear.new("Yogi", [])
-    @bear2 = Bear.new("Grizzly", [])
-    @bear3 = Bear.new("Carebear", [])
+
 #I am not convinced about adding in an array for the stomach as an argument. Do I need to do this?
     @fish1 = Fish.new("Trout")
     @fish2 = Fish.new("Salmon")
     @fish3 = Fish.new("Sturgeon")
     @all_the_fish = [@fish1, @fish2, @fish3]
+
+    @bear1 = Bear.new("Yogi")
+    @bear2 = Bear.new("Grizzly")
+    @bear3 = Bear.new("Carebear", @all_the_fish)
+
 
     @river = River.new("Yukon", @all_the_fish)
   end
@@ -26,39 +29,59 @@ class BearTest < MiniTest::Test
   end
 
   def test_eat_a_fish()
-    result1 = @bear1.eat_a_fish(@fish1)
-    assert_equal([@fish1], result1)
-    result2 = @bear1.eat_a_fish(@fish2)
-    assert_equal([@fish1, @fish2], result2)
-    result3 = @bear2.eat_a_fish(@fish3)
-    assert_equal([@fish3], result3)
+    @bear1.eat_a_fish(@fish1)
+    assert_equal(1, @bear1.stomach.count)
+
   end
 
+#test for this by counting the length of the stomach array - testing for eat_a_fish @bear.stomach.length()
+
   def test_is_starving__true()
-    assert_equal(true, @bear1.is_starving)
+    assert_equal(true, @bear1.is_starving?)
   end
 
   def test_is_starving__false()
-    full_bear = @bear1.eat_a_fish(@fish1)
-    assert_equal(false, full_bear.empty?)
+    assert_equal(false, @bear3.is_starving?)
   end
 
-  def test_eat_fish_from_river__no_fish()
+  def test_hunt_for_fish__fish_in_river()
     @bear1.hunt_for_fish(@river)
-    result = @bear1
-    #full_bear = @bear1.eat_a_fish(@fish1)
-    assert_equal(true, result )#full_bear.empty?)
+    assert_equal(1, @bear1.stomach.count)
   end
 
-  def test_eat_fish_from_river__fish()
-
+  def test_hunt_for_fish__no_fish()
+    empty_river = River.new("Polluted", [])
+    @bear1.hunt_for_fish(empty_river)
+    assert_equal(0, @bear1.stomach.count)
   end
+
+
 # A bear should be able to eat a fish from the river. Bear will have a function hunt_for_fish( River ) which takes a River pbject as an argument. Inside it will check if there is any fish in the river, take the first fish and bear will eat that fish
 
-  def test_digest__bear_stomach_full()
-    full_bear = Bear.new("Fatty", [@fish1, @fish2])
-    assert_equal([], full_bear.digest)
-  end
+
+
+
+
+
+
+
+
+
+  # def test_eat_fish_from_river__no_fish()
+  #   @bear1.hunt_for_fish(@river)
+  #
+  #   assert_equal(true, @bear1.is_starving? )#full_bear.empty?)
+  #  end
+  #
+  # def test_eat_fish_from_river__fish()
+  #
+  # end
+
+  #
+  # def test_digest__bear_stomach_full()
+  #   full_bear = Bear.new("Fatty")
+  #   assert_equal([], full_bear.digest)
+  # end
 
 #I tried to call the eat_a_fish method on a bear object, but digest does not work for what is returned from eat_a_fish (an array), so I've had to add in a second argument to my bear object.
 
